91df4f296480e4b47444155b077c10cf932dba76
Anish Dutta <60481056+anishdutta@users.noreply.github.com>
2020-04-04 18:19:36 +0530

    delay scroll

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-
+var x =0;
 $(window).scroll( function(){
-    
+        if(x>30){
         $(".dash-img").hide( "fold", {horizFirst: true }, 3500 );
-    
-   });
+        }
+        else{
+                x+=1;
+        }
+        
+    }) ;
    
