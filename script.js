18c2cc39e33bd77dd741509eee8e7c03474b805d
Anish Dutta <60481056+anishdutta@users.noreply.github.com>
2020-04-08 23:36:34 +0530

    comments

--- script.js
+++ script.js
@@ -2,14 +2,17 @@ var x =0;
 $(window).scroll( function(){
 	}) ;
 	
-	
+	/* dash-img= dash image
+	pro-gif= gif 0f dash
+	svg= animated wifi
+	app-img= app screenshot*/
 	var lastScrollTop = 0;
     $(window).on('scroll', function() {
         st = $(this).scrollTop();
         if(st == 0) {
-			$(".dash-img").show( "slide", {direction: "up"}, 2000 );
-			$(".pro-gif").hide( "slide", {direction: "left" }, 2000 );
-			$("svg").hide("clip",{direction: "vertical"}, 2500);
+			$(".dash-img").show( "slide", {direction: "up"}, 2000/*speed(ms)*/ );
+			$(".pro-gif").hide( "slide", {direction: "left" }, 2000/*speed*/ );
+			$("svg").hide("clip",{direction: "vertical"}, 2500/*speed*/);
                         $(".app-img").hide( "slide", {direction: "right"}, 2500 );
 
 
