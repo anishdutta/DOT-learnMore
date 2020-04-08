8f7e1663d89e267ac5d61597894b7519cc887f20
Anish Dutta <60481056+anishdutta@users.noreply.github.com>
2020-04-08 20:45:33 +0530

    animation

--- script.js
+++ script.js
@@ -1,12 +1,32 @@
 var x =0;
-$(window).( function(){
-        if(x>10){
-        $(".dash-img").hide( "fold", {horizFirst: false }, 3500 );
+$(window).scroll( function(){
+	}) ;
+	
+	
+	var lastScrollTop = 0;
+    $(window).on('scroll', function() {
+        st = $(this).scrollTop();
+        if(st == 0) {
+			$(".dash-img").show( "slide", {direction: "up"}, 2000 );
+			$(".pro-gif").hide( "slide", {direction: "left" }, 2000 );
+			$("svg").hide("clip",{direction: "vertical"}, 2500);
+                        $(".app-img").hide( "slide", {direction: "right"}, 2500 );
+
+
         }
-        else{
-                x+=1;
+        else {
+			$(".dash-img").hide( "slide", {direction: "up"}, 2000 );
+			$(".pro-gif").show( "slide", {direction: "left" }, 2000 );
+                        $(".app-img").show( "slide", {direction: "right"}, 2500 );
+			$("svg").show("clip",{direction: "vertical"}, 2500);
+
+
         }
-        
-    }) ;
-    
-    
+		lastScrollTop = st;
+		
+		
+
+	})
+	$(document).ready(function(){
+		$(".down-arrow").effect( "bounce", {times:20}, 10000 );
+	})
