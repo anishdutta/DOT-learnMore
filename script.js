beb9fae5d26a49f28c3831870d95b9db78a8e175
Anish Dutta <60481056+anishdutta@users.noreply.github.com>
2020-04-13 19:13:14 +0530

    anim

--- script.js
+++ script.js
@@ -9,15 +9,8 @@ $(window).scroll( function(){
 	var lastScrollTop = 0;
     $(window).on('scroll', function() {
         st = $(this).scrollTop();
-        if(st == 0) {
-			$(".dash-img").show( "slide", {direction: "up"}, 2000/*speed(ms)*/ );
-			$(".pro-gif").hide( "slide", {direction: "left" }, 2000/*speed*/ );
-			$("svg").hide("clip",{direction: "vertical"}, 2500/*speed*/);
-                        $(".app-img").hide( "slide", {direction: "right"}, 2500 );
-
-
-        }
-        else {
+        if(st > 50) {
+		
 			$(".dash-img").hide( "slide", {direction: "up"}, 2000 );
 			$(".pro-gif").show( "slide", {direction: "left" }, 2000 );
                         $(".app-img").show( "slide", {direction: "right"}, 2500 );
